15328efb8f91f9e11062db1559157432856cffa7
alvinC <lvinz0790@gmail.com>
2022-03-01 14:02:59 -0800

    fetch basic json

---
+++ fetchjson/index.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+axios_1["default"].get(url)
+    .then(function (response) {
+    // const todo = response.data as Todo;
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    logTodo(id, title, completed);
+});
+var logTodo = function (id, title, completed) {
+    console.log("\n            The Todo with ID: " + id + "\n            Has a title of: " + title + "\n            Is it finished? " + completed + "\n        ");
+};
