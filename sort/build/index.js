3663b61f9ec0ff2e59c060950a51c465c54067db
alvinC <lvinz0790@gmail.com>
2022-03-21 21:40:30 -0700

    sort setup

---
+++ sort/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello World");
+var logSomething = function () {
+    console.log("Something");
+};
+logSomething();
---
+++ sort/src/index.ts
@@ -0,0 +1,7 @@
+console.log("Hello World");
+
+const logSomething = () => {
+     console.log("Something")
+}
+
+logSomething();
