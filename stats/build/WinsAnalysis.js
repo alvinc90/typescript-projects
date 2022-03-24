c06bd13d449cf749d1e5c447342cde33c93cb88d
alvinC <lvinz0790@gmail.com>
2022-03-24 11:48:49 -0700

    football-stats-csv-inheritance-composition

---
+++ stats/build/WinsAnalysis.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WinsAnalysis = void 0;
+var WinsAnalysis = /** @class */ (function () {
+    function WinsAnalysis() {
+    }
+    return WinsAnalysis;
+}());
+exports.WinsAnalysis = WinsAnalysis;
---
+++ stats/src/index.ts
@@ -0,0 +1,11 @@
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
+
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.WinsAnalysisWithHtmlReport("Man United");
+
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
+
+
+
