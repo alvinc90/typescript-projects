afeab9f08d723f461a4dd763d1239555a688d704
alvinC <lvinz0790@gmail.com>
2022-04-09 20:03:41 -0700

    Revert "metadata"
    This reverts commit bb768e64076305fd521f2027490b9260becea184.

--- features/annotations/variable.ts
+++ features/annotations/variable.ts
@@ -9,36 +9,37 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 // Array
-let colors: string[] = ["red", "green", "blue"];
-let myNumbers: number[] = [1, 2, 3];
+let colors:  string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1,2,3];
 let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {}
+class Car { };
 let car: Car = new Car();
 
 // Object literal
 let point: { x: number; y: number } = {
-  x: 10,
-  y: 20,
+    x: 10,
+    y: 20,
 };
 
 // Function
 const logNumber: (i: number) => void = (i: number) => {
-  console.log(i);
-};
+    console.log(i);
+}
 
 // 1) Any
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // { x: 10, y: 20 };
 
+
 // 2) Any
 let words = ["red", "green", "blue"];
 let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
-  if (words[i] === "green") foundWord = true;
+    if (words[i] === "green") foundWord = true;
 }
 
 // 3) Any
@@ -46,7 +47,7 @@ let numbers = [-10, -1, 12];
 let numberAboveZero: boolean | number = false;
 
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > 0) {
-      numberAboveZero = numbers[i];
-  }
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
 }
